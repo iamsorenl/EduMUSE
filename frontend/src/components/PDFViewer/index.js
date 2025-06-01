a3d03cfcfda831b3e265f9435cd13e40500c6b53
Soren Larsen <iamsorenl@gmail.com>
2025-06-01 16:03:38 -0700

    made a new branch to put my agent on to

--- frontend/src/components/PDFViewer/index.js
+++
@@ -1,6 +0,0 @@
-export { default } from './PDFViewer';
-export { default as PDFControls } from './PDFControls';
-export { default as PDFDocument } from './PDFDocument';
-export { default as PDFActionButtons } from './PDFActionButtons';
-export { default as SelectedTextDisplay } from './SelectedTextDisplay';
-export { default as EmptyState } from './EmptyState';
